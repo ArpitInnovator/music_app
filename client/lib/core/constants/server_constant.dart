3375337035ad28865ab715d5a90b4def974290b4
Arpit <arpitprofessional04@gmail.com>
2026-01-04 18:50:07 +0530

    Updating Deployed backend url

--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -1,18 +1,4 @@
-import 'package:flutter/foundation.dart';
-import 'dart:io' show Platform;
-
 class ServerConstant {
-  static String get serverURL {
-    if (kIsWeb) {
-      return 'http://localhost:8000';
-    }
-
-    // Android Emulator
-    if (Platform.isAndroid) {
-      return 'http://192.168.29.33:8000';
-    }
-
-    // Fallback (iOS / others)
-    return 'http://localhost:8000';
-  }
+  static const String serverURL =
+      'https://music-app-ig2e.onrender.com';
 }
